2951b39d748e33bc6a20627e16346fba8a6769b0
Jörg <joerg.landthaler@fylance.de>
2021-09-17 20:14:18 +0200

    Hello World version

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from configparser import ConfigParser
-
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
+# from configparser import ConfigParser
+#
+# from elasticsearch import Elasticsearch
+# from elasticsearch_dsl import Search
 from flask import Flask
 
-# Configuration
-config = ConfigParser()
-config.read('configuration.ini')
-
-# Query
-query_definition = {
-    "bool": {
-        "must": [{'gender': 'female'}]
-}}
+# # Configuration
+# config = ConfigParser()
+# config.read('configuration.ini')
+#
+# # Query
+# query_definition = {
+#     "bool": {
+#         "must": [{'gender': 'female'}]
+# }}
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return "Hello world!"
 
-    es_client =  Elasticsearch([{ 'host': config.get('DEFAULT', 'host'), 'port': config.get('DEFAULT', 'port') }])
-
-    query_object = Search()
-    query_object = query_object.query(query_definition)
-    query_object = query_object.using(es_client)
-    query_object = query_object.index(config.get('DEFAULT', 'index'))
-    query_result = query_object.execute()
-
-    return query_result.to_dict()
+# @app.route("/")
+# def index():
+#
+#     es_client =  Elasticsearch([{ 'host': config.get('DEFAULT', 'host'), 'port': config.get('DEFAULT', 'port') }])
+#
+#     query_object = Search()
+#     query_object = query_object.query(query_definition)
+#     query_object = query_object.using(es_client)
+#     query_object = query_object.index(config.get('DEFAULT', 'index'))
+#     query_result = query_object.execute()
+#
+#     return query_result.to_dict()
